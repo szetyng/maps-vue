0d75c068dc2c1a4c4f3c9ef122f528331e08e56b
Sze Tyng Lee <lee.szetyng@gmail.com>
2020-09-25 11:07:22 +0800

    toggle layers

--- pages/openlayers.vue
+++ pages/openlayers.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <v-btn @click="s776Show = !s776Show">
+      Show S776
+    </v-btn>
+    <v-btn @click="s777Show = !s777Show">
+      Show S777
+    </v-btn>
     <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" style="height: 600px">
       <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
 
@@ -7,7 +13,7 @@
         <vl-source-osm></vl-source-osm>
       </vl-layer-tile>
 
-      <vl-interaction-select :features.sync="selectedFeatures" :condition="eventCondition">
+      <vl-interaction-select :features.sync="selectedFeatures" :condition="featureSelect">
         <template slot-scope="select">
           <vl-style-box>
             <vl-style-stroke color="#423e9e" :width="7"></vl-style-stroke>
@@ -57,12 +63,22 @@
         </template>
       </vl-interaction-select>
 
-      <vl-layer-vector>
+      <vl-layer-vector v-if="s776Show">
         <vl-source-vector :features="getTunnel"></vl-source-vector>
         <vl-style-box>
-          <vl-style-circle :radius="2">
+          <vl-style-circle :radius="5">
             <vl-style-fill color="blue"></vl-style-fill>
             <vl-style-stroke color="blue"></vl-style-stroke>
+          </vl-style-circle>
+          </vl-style-box>
+      </vl-layer-vector>
+      
+      <vl-layer-vector v-if="s777Show">
+        <vl-source-vector :features="getTunnel2"></vl-source-vector>
+        <vl-style-box>
+          <vl-style-circle :radius="5">
+            <vl-style-fill color="red"></vl-style-fill>
+            <vl-style-stroke color="red"></vl-style-stroke>
           </vl-style-circle>
           </vl-style-box>
       </vl-layer-vector>
@@ -77,15 +93,18 @@
           </vl-style-box>
       </vl-feature> -->
     </vl-map>
-    Selected feature: {{ selectedFeatures }}
+    Selected feature: {{ selectedFeatures }} 
+    <br>
+    Hovered feature [disabled]: {{ hoveredFeatures }}
   </div>
 </template>
 
 <script>
 import { fromLonLat } from 'ol/proj';
-import { pointerMove } from 'ol/events/condition';
+import { pointerMove, singleClick } from 'ol/events/condition';
 import { findPointOnSurface } from 'vuelayers/lib/ol-ext'
 import s776Rings from '../assets/s776rings.json'
+import s777Rings from '../assets/s777rings.json'
 
 export default {
   data() {
@@ -97,7 +116,11 @@ export default {
       // notcenter: fromLonLat([101.7170222, 3.129455556]),
       rotation: 0,
       s776RingLoc: s776Rings,
-      selectedFeatures: []
+      s777RingLoc: s777Rings,
+      selectedFeatures: [],
+      hoveredFeatures: [],
+      s776Show: true,
+      s777Show: false
     }
   },
   mounted() {
@@ -127,8 +150,31 @@ export default {
       return tunnel
     },
 
-    eventCondition() {
+    getTunnel2() {
+      let tunnel = this.s777RingLoc.map(ringObj => {
+        let featureObj = {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: fromLonLat([ringObj.long, ringObj.lat])
+          },
+          properties: {
+            name: `R${ringObj.Ring}`
+
+          }
+        }
+        return featureObj
+      });
+
+      return tunnel
+    },
+
+    featureHover() {
       return pointerMove
+    },
+
+    featureSelect() {
+      return singleClick
     }
   
   }
