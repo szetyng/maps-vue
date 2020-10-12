8647217cfd591958b9797a429483ce0570e4567d
Sze Tyng Lee <lee.szetyng@gmail.com>
2020-10-12 13:48:15 +0800

    toggling tunnels in vanilla openlayers

--- pages/vanilla-layers.vue
+++ pages/vanilla-layers.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <v-btn @click="toggle776">
+      Show S776
+    </v-btn>
+    <v-btn @click="toggle777">
+      Show S777
+    </v-btn>
     <div id="map" class="map"></div>
   </div>
 </template>
@@ -31,47 +37,90 @@ export default {
   },
   methods: {
     draw776() {
+      var image = new CircleStyle({
+        radius: 5,
+        fill: new Fill({ color: "red" }),
+        stroke: new Stroke({ color: "red", width: 1 }),
+      });
+
+      var styles = {
+        Point: new Style({
+          image: image,
+        }),
+      };
+
+      var styleFunction = function (feature) {
+        return styles[feature.getGeometry().getType()];
+      };
+
       let source776 = new VectorSource({
-        features: new GeoJSON().readFeatures(this.s776Tunnel),
+        features: new GeoJSON({ featureProjection: "EPSG:3857" }).readFeatures(this.s776Tunnel),
       });
 
       let layer776 = new VectorLayer({
         source: source776,
+        style: styleFunction,
       });
 
+      this.layer776 = layer776;
       this.mymap.addLayer(layer776);
+    
     },
+    draw777() {
+      var image = new CircleStyle({
+        radius: 5,
+        fill: new Fill({ color: "blue" }),
+        stroke: new Stroke({ color: "blue", width: 1 }),
+      });
+
+      var styles = {
+        Point: new Style({
+          image: image,
+        }),
+      };
+
+      var styleFunction = function (feature) {
+        return styles[feature.getGeometry().getType()];
+      };
+
+      let source777 = new VectorSource({
+        features: new GeoJSON({ featureProjection: "EPSG:3857" }).readFeatures(this.s777Tunnel),
+      });
+
+      let layer777 = new VectorLayer({
+        source: source777,
+        style: styleFunction,
+      });
+
+      this.layer777 = layer777;
+      this.mymap.addLayer(layer777);
+    },
+    toggle776() {
+      this.s776Show = !this.s776Show;
+
+      if (this.s776Show) {
+        this.mymap.addLayer(this.layer776);
+      } else {
+        this.mymap.removeLayer(this.layer776);
+      }
+    },
+
+    toggle777() {
+      this.s777Show = !this.s777Show;
+
+      if (this.s777Show) {
+        if (this.layer777 == null) {
+          this.draw777();
+          return
+        }
+        this.mymap.addLayer(this.layer777);
+      } else {
+        this.mymap.removeLayer(this.layer777);
+      }
+    }
   },
 
   mounted() {
-    var image = new CircleStyle({
-      radius: 5,
-      fill: new Fill({ color: "red" }),
-      stroke: new Stroke({ color: "red", width: 1 }),
-    });
-
-    var styles = {
-      Point: new Style({
-        image: image,
-      }),
-    };
-
-    var styleFunction = function (feature) {
-      return styles[feature.getGeometry().getType()];
-    };
-    //this.mymap = map;
-
-    let source776 = new VectorSource({
-      //format: new GeoJSON(),
-      features: new GeoJSON({ featureProjection: "EPSG:3857" }).readFeatures(
-        this.s776Tunnel
-      ),
-    });
-
-    let layer776 = new VectorLayer({
-      source: source776,
-      style: styleFunction,
-    });
 
     // map.addLayer(layer776);
     // map.render();
@@ -81,14 +130,20 @@ export default {
       layers: [
         new TileLayer({
           source: new OSM(),
-        }),
-        layer776,
+        })
       ],
       view: new View({
         center: fromLonLat([101.7170167, 3.129447222]),
         zoom: 18,
       }),
     });
+
+
+
+    this.mymap = map;
+    this.draw776();
+
+    
   },
 };
 </script>
